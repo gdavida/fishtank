ea35f8560c69a9cbc4bdf8bcb375bbd7474d3ee7
Davida Gaffney <gdavida@gmail.com>
2016-05-03 21:41:38 -0500

    validations and tests for Tank

--- app/models/tank.rb
+++ app/models/tank.rb
@@ -1,2 +1,5 @@
 class Tank < ActiveRecord::Base
+	validates :name, presence:true
+	validates :rock_color, presence:true
+	validates :shelter, presence:true
 end
--- test/models/tank_test.rb
+++ test/models/tank_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class TankTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	t = Tank.new(name: "Aqueon", rock_color: "purple", shelter: "castle")
+  
+  test "should be valid" do
+  	t = Tank.new(name: "Aqueon", rock_color: "purple", shelter: "castle")
+    assert t.valid?
+  end
+
+  test "name should be present" do
+  	t.name = ""
+    refute t.valid?
+  end
+
+  test "rock color should be present" do
+  	t.rock_color = ""
+    refute t.valid?
+  end
+
+  test "shelter should be present" do
+  	t.shelter = ""
+    refute t.valid?
+  end
 end
